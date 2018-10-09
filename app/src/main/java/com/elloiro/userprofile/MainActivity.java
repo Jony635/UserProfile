f076279a2257953e70d5ee945e41e728d61b2e73
Jony635 <jony635molina@gmail.com>
2018-10-09 17:06:54 +0200

    Initial commit

---
+++ app/src/main/java/com/elloiro/userprofile/MainActivity.java
@@ -0,0 +1,70 @@
+package com.elloiro.userprofile;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Profile profile;
+    private Gson gson;
+    private TextView name;
+    private TextView handle;
+    private TextView following;
+    private TextView followers;
+    private TextView about;
+    private ImageView background;
+    private ImageView profileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gson = new Gson();
+
+        try {
+            InputStream stream = getAssets().open("Me.json");
+            InputStreamReader reader = new InputStreamReader(stream);
+            profile = gson.fromJson(reader, Profile.class);
+        } catch (IOException e) {
+            Toast.makeText(this,"json couldn't be read", Toast.LENGTH_LONG).show();
+        }
+
+        name = findViewById(R.id.name);
+        handle = findViewById(R.id.handle);
+        following = findViewById(R.id.FollowingNumber);
+        followers = findViewById(R.id.FollowersNumber);
+        about = findViewById(R.id.AboutText);
+        background = findViewById(R.id.ImageBackground);
+        profileImage = findViewById(R.id.ImageProfile);
+
+
+        updateProfile();
+
+    }
+    private void updateProfile() {
+        name.setText(profile.getName() + " " + profile.getLastname());
+        handle.setText(profile.getHandle());
+        following.setText(profile.getFollowing());
+        followers.setText(profile.getFollowers());
+        about.setText(profile.getAbout());
+
+        Glide.with(this).load("file:///android_asset/profileImage.jpg").into(profileImage);
+        Glide.with(this).load("file:///android_asset/backgroundImage.jpg").into(background);
+    }
+}
